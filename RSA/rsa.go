149058e1a6e9fccce2af3e866ebb126a2beba8d2
au598835 <vitus.bjoern@gmail.com>
2020-09-28 12:55:04 +0200

    Refactored project structure

---
+++ RSA/rsa.go
@@ -0,0 +1,22 @@
+package RSA
+
+import (
+	"math/big"
+)
+
+// Der findes en funktion Prime i pakken crypto/rand
+
+// Public key
+var e = 3
+
+func KeyGen(k big.Int) {
+
+}
+
+func Encrypt() {
+
+}
+
+func Decrypt() {
+
+}
