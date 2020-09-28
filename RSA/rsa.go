b29f218867874c0a6221890b09e7fb9c724c7729
au598835 <vitus.bjoern@gmail.com>
2020-09-28 14:53:09 +0200

    Added a few more debug prints, no fixes

--- RSA/rsa.go
+++ RSA/rsa.go
@@ -1,33 +1,132 @@
 package RSA
 
 import (
+	"crypto/rand"
+	"fmt"
 	"math/big"
+	"strconv"
 )
 
-// Opgavebeskrivelsen siger vi skal bruge integers fra math/big pakken
+func Run() {
+	fmt.Println("hej")
 
-// Public key is (e, n)
-// Private key is (d, n)
+	k := 12
+	pk, sk := KeyGen(k)
 
-// e is chosen in the assignment description
-var e = 3
+	fmt.Println("Private key, n = ", pk.N, "e = ", pk.E)
+	fmt.Println("Secret key, n = ", sk.N, "d = ", sk.D)
 
-func KeyGen(k big.Int) {
-	// Choose two primes p and q
-	// It must hold for p and q that gcd(e, p-1) = gcd(e, q-1) = 1, where gcd() is greatest common divisor
+	if k == pk.N.BitLen() {
+		fmt.Println("n has length k: k = " + strconv.Itoa(k) + ", len(n) = " + strconv.Itoa(pk.N.BitLen()))
+	} else {
+		fmt.Println("n does not have length k: k = " + strconv.Itoa(k) + ", len(n) = " + strconv.Itoa(pk.N.BitLen()))
+	}
 
-	// n = p*q   ...  n must have length k
-	// d = e^(-1) % (p-1)(q-1)
+	fmt.Println()
+
+	fmt.Println("Encrypt: ")
+	m := big.NewInt(64)
+	fmt.Println("Length of m is: " + strconv.Itoa(m.BitLen()))
+	c := Encrypt(pk, m)
+	fmt.Println("Cipher is: ", c)
+
+	fmt.Println("Decrypted Cipher: ", Decrypt(sk, c))
+
 }
 
-// Encrypt takes som number m that is in the interval [0 .. m-1] and outputs a number c
-func Encrypt(m big.Int) big.Int {
-	// c = m^e % n
-	return *big.NewInt(0)
+/*
+	RSA: public key consist of two numbers n and e, and the private key consists of two numbers n and d.
+
+	n is 'modulus' and is the product of two prime numbers p,q.
+	e most be chosen such that, gcd(e) = gcd(p-1) = gcd(q-1) = 1
+	Then d is computed to satisfy: e * d mod (p-1)(q-1) = 1, gives: d = e^(-1) mod (p-1)(q-1)
+
+	To encrypt: c = m^e mod n
+	To decrypt: m = c^d mod n
+*/
+
+type PublicKey struct {
+	N *big.Int
+	E *big.Int
 }
 
-// Decrypt takes some number c and outputs some number m
-func Decrypt(c big.Int) big.Int {
-	// m = c^d mod n
-	return *big.NewInt(0)
+type SecretKey struct {
+	N *big.Int
+	D *big.Int
+}
+
+// k is the bit length of the generated modulus, n = pq, k=len(n), gives a precondition that k > 3
+func KeyGen(k int) (PublicKey, SecretKey) {
+	// choose e = 3
+	e := big.NewInt(3)
+
+	// Choose p,q as primes such that bitLen(p*q) = k
+	// a * b = c, len(c) = len(a+b), if a,b,c is binary numbers
+
+	// Bit length of p, is chosen to be k/2
+	lenP := k / 2
+	// Length of q, is then len(k)-len(p)
+	lenQ := k - lenP
+
+	// choose Primes for p and q
+	p, _ := rand.Prime(rand.Reader, lenP)
+	q, _ := rand.Prime(rand.Reader, lenQ)
+
+	fmt.Println("p is: ", p)
+	fmt.Println("q is: ", q)
+
+	// n = p * q
+	n := new(big.Int).Mul(p, q)
+
+	fmt.Println("n is: ", n)
+
+	// d is defined as: d = e^(-1) mod (p-1)(q-1)
+	// t1 = (p-1)
+	t1 := new(big.Int).Sub(p, big.NewInt(1))
+	// t2 = (q-1)
+	t2 := new(big.Int).Sub(q, big.NewInt(1))
+	// t3 = t1*t2 = (p-1)(q-1)
+	t3 := new(big.Int).Mul(t1, t2)
+	// d = e modinverse t3 = e^(-1) mod (p-1)(q-1)
+	d := new(big.Int).ModInverse(e, t3)
+
+	fmt.Println("d is: ", d)
+
+	// Create public and secret key
+	pk := new(PublicKey)
+	pk.E = e
+	pk.N = n
+
+	sk := new(SecretKey)
+	sk.D = d
+	sk.N = n
+
+	return *pk, *sk
+}
+
+func Encrypt(pk PublicKey, m *big.Int) *big.Int {
+	// To encrypt: c = m^e mod n
+	// m has to be in the interval [0 .. n-1]
+	if m.Cmp(new(big.Int).Sub(pk.N, big.NewInt(1))) > 0 {
+		fmt.Println("Error: m is too large, cannot encode")
+		return big.NewInt(-1)
+	}
+
+	// calculate m^e
+	t := new(big.Int).Exp(m, pk.E, nil)
+	// calculate t mod n = m^e mod n
+	c := new(big.Int).Mod(t, pk.N)
+
+	return c
+}
+
+func Decrypt(sk SecretKey, c *big.Int) *big.Int {
+	// To decrypt: m = c^d mod n
+
+	// calculate c^d
+	t := new(big.Int).Exp(c, sk.D, nil)
+	// Calculate t mod n = c^d mod n
+	m := new(big.Int).Mod(t, sk.N)
+
+	return m
 }
--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "./RSA"
+
 func main() {
 	// peer2peer.start()
+	RSA.Run()
 }
