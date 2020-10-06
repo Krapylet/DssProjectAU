2dfd9d0b3bb646735b322ef0a9e8cd81f40673e5
Mikkel Tønner Hansen <201808415@post.au.dk>
2020-10-06 12:11:32 +0200

    refactored

--- RSA/main.go
+++ RSA/main.go
@@ -1,14 +1,11 @@
-package main
+package RSA
 
 import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
-	"io"
 	"math/big"
 	"strconv"
-	"time"
 )
 
 type PublicKey struct {
@@ -98,75 +95,6 @@ func Decrypt(sk SecretKey, cipher big.Int) *big.Int {
 
 // --- RSA Signatures ---
 
-func main() {
-
-	pk, sk := KeyGen(2000)
-
-	// 10 kB of random data
-	msg := make([]byte, 10000)
-	io.ReadFull(rand.Reader, msg)
-
-	// 2. Time for hashing
-	start := time.Now()
-	// Make hash of msg
-	hash := big.NewInt(makeSHA256(msg))
-	elapsed := time.Since(start)
-	fmt.Println("Time spent hashing: ", elapsed)
-	fmt.Println("BitsPerSec = ", 10000/elapsed.Seconds())
-	//  Around 315.786.149 bits per sec
-
-	// 3. Time for RSA using the hash value
-	start = time.Now()
-	s := Sign(*hash, sk)
-	elapsed = time.Since(start)
-	fmt.Println("Time spent signing (hash): ", elapsed)
-	fmt.Println("BitsPerSec = ", 10000/elapsed.Seconds())
-	// Around 2.520.726 bits per sec
-
-	// 3.
-	msg1 := make([]byte, 1999)
-	msg2 := make([]byte, 1999)
-	msg3 := make([]byte, 1999)
-	msg4 := make([]byte, 1999)
-	msg5 := make([]byte, 1999)
-	io.ReadFull(rand.Reader, msg1)
-	io.ReadFull(rand.Reader, msg2)
-	io.ReadFull(rand.Reader, msg3)
-	io.ReadFull(rand.Reader, msg4)
-	io.ReadFull(rand.Reader, msg5)
-
-	msg1Hex := hex.EncodeToString(msg1)
-	msg2Hex := hex.EncodeToString(msg2)
-	msg3Hex := hex.EncodeToString(msg3)
-	msg4Hex := hex.EncodeToString(msg4)
-	msg5Hex := hex.EncodeToString(msg5)
-
-	msg1Int, _ := strconv.ParseInt(msg1Hex, 16, 64)
-	msg2Int, _ := strconv.ParseInt(msg2Hex, 16, 64)
-	msg3Int, _ := strconv.ParseInt(msg3Hex, 16, 64)
-	msg4Int, _ := strconv.ParseInt(msg4Hex, 16, 64)
-	msg5Int, _ := strconv.ParseInt(msg5Hex, 16, 64)
-	fmt.Println(msg1Int)
-	start = time.Now()
-	Sign(*big.NewInt(msg1Int), sk)
-	Sign(*big.NewInt(msg2Int), sk)
-	Sign(*big.NewInt(msg3Int), sk)
-	Sign(*big.NewInt(msg4Int), sk)
-	Sign(*big.NewInt(msg5Int), sk)
-	elapsed = time.Since(start)
-	fmt.Println("Time spent signing (no hash): ", elapsed)
-	fmt.Println("BitsPerSec = ", 10000/elapsed.Seconds())
-
-	// modify msg
-	modMsg := new(big.Int).Add(hash, big.NewInt(1))
-
-	fmt.Println("msg: ", hash)
-	fmt.Println("Modified msg: ", modMsg)
-	fmt.Println("Signed: ", s)
-	fmt.Println("Verified msg: ", Verify(*s, *hash, pk))
-	fmt.Println("Verified modMsg: ", Verify(*s, *modMsg, pk))
-}
-
 /*
 	Sign: Sign the message using s = m^d mod n,
 	where msg = original msg, sk = secret key,
@@ -196,7 +124,7 @@ func Verify(s big.Int, msg big.Int, pk PublicKey) bool {
 	return false
 }
 
-func makeSHA256(msg []byte) int64 {
+func MakeSHA256(msg []byte) int64 {
 	hash := sha256.Sum256(msg)
 
 	// Convert to Hex
