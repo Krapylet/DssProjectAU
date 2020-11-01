9b25201d93cd301b42777f21646c97d591992f95
Mikkel Tønner Hansen <201808415@post.au.dk>
2020-11-01 14:30:31 +0100

    d

--- handin7/peer2peer/softwarewallet/softwarewallet.go
+++ handin7/peer2peer/softwarewallet/softwarewallet.go
@@ -9,14 +9,12 @@ import (
 
 /*
 	Type of signature - string
- */
-
+*/
 
 // Password - should have byte len 32
 
 func Generate(filename string, password string) string {
-	// TODO: noget hashing af password så den får byte len 32,
-	// TODO: ved ikke om vi må bruge bcrypt eller vi selv skal implementere noget selv
+	// TODO: Ved ikke hvad der skal gøres ved password
 
 	// TODO: Tager de 32 første bytes af sha256, skal nok laves om
 	pwHash32 := []byte(RSA.MakeSHA256Hex([]byte(password)))[0:32]
@@ -25,17 +23,16 @@ func Generate(filename string, password string) string {
 	pk, sk := RSA.KeyGen(2048)
 
 	// make sk to string "n:d"
-	skAsString := sk.N.String() + ":" +sk.D.String()
+	skAsString := sk.N.String() + ":" + sk.D.String()
 
 	// Encrypt to file
-	AES.EncryptToFile(filename, []byte (skAsString), pwHash32)
+	AES.EncryptToFile(filename, []byte(skAsString), pwHash32)
 
 	// return public key, as string "n:e"
 	pkAsString := pk.N.String() + ":" + pk.E.String()
 
 	return pkAsString
 }
-
 
 func Sign(filename string, password string, msg []byte) string {
 
@@ -44,7 +41,6 @@ func Sign(filename string, password string, msg []byte) string {
 
 	skAsString := string(AES.DecryptFromFile(filename, pwHash32))
 	splitSK := strings.Split(string(skAsString), ":")
-
 
 	// TODO: En eller anden test, der verificerer om det er en gyldig SK, der er blevet decrypted
 	skN, err1 := new(big.Int).SetString(splitSK[0], 10)
@@ -56,7 +52,6 @@ func Sign(filename string, password string, msg []byte) string {
 	sk := new(RSA.SecretKey)
 	sk.N = skN
 	sk.D = skD
-
 
 	msgToBigInt := new(big.Int).SetBytes(msg)
 
