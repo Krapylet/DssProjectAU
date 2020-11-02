07b4eb547d69ef321848c749e29f5ba0444b621d
Mikkel Tønner Hansen <201808415@post.au.dk>
2020-11-02 11:48:59 +0100

    sha hashing of pw

--- handin7/peer2peer/softwarewallet/softwarewallet.go
+++ handin7/peer2peer/softwarewallet/softwarewallet.go
@@ -3,8 +3,10 @@ package softwarewallet
 import (
 	"../../AES"
 	"../../RSA"
+	"encoding/json"
+	"fmt"
 	"math/big"
-	"strings"
+	"time"
 )
 
 /*
@@ -14,19 +16,31 @@ import (
 // Password - should have byte len 32
 
 func Generate(filename string, password string) string {
-	// TODO: Ved ikke hvad der skal gøres ved password
 
-	// TODO: Tager de 32 første bytes af sha256, skal nok laves om
-	pwHash32 := []byte(RSA.MakeSHA256Hex([]byte(password)))[0:32]
+	// pwHash32 := []byte(RSA.MakeSHA256Hex([]byte(password)))[0:32]
+
+	// Hash the password 2.000.000 times...
+	hashedPW := password
+	start := time.Now()
+	for i := 0; i < 2000000; i++ {
+		hashedPW = RSA.MakeSHA256Hex([]byte(hashedPW))
+	}
+	elapsed := time.Since(start)
+	fmt.Println("elapsed:", elapsed)
+	// get first 32 bytes of hashed pw
+	pwHash32 := []byte(hashedPW)[0:32]
 
 	// Generate keys
 	pk, sk := RSA.KeyGen(2048)
 
-	// make sk to string "n:d"
-	skAsString := sk.N.String() + ":" + sk.D.String()
+	// Json Marshal the SK
+	toEncrypt, err := json.Marshal(sk)
+	if err != nil {
+		panic(err)
+	}
 
 	// Encrypt to file
-	AES.EncryptToFile(filename, []byte(skAsString), pwHash32)
+	AES.EncryptToFile(filename, toEncrypt, pwHash32)
 
 	// return public key, as string "n:e"
 	pkAsString := pk.N.String() + ":" + pk.E.String()
@@ -36,26 +50,25 @@ func Generate(filename string, password string) string {
 
 func Sign(filename string, password string, msg []byte) string {
 
-	// takes first 32-bytes of the hashed pw
-	pwHash32 := []byte(RSA.MakeSHA256Hex([]byte(password)))[0:32]
-
-	skAsString := string(AES.DecryptFromFile(filename, pwHash32))
-	splitSK := strings.Split(string(skAsString), ":")
-
-	// TODO: En eller anden test, der verificerer om det er en gyldig SK, der er blevet decrypted
-	skN, err1 := new(big.Int).SetString(splitSK[0], 10)
-	skD, err2 := new(big.Int).SetString(splitSK[1], 10)
-	if err1 == false && err2 == false {
-		panic("Decrypt failed: Wrong password")
+	// Hash the password 50 times...
+	hashedPW := password
+	for i := 0; i < 2000000; i++ {
+		hashedPW = RSA.MakeSHA256Hex([]byte(hashedPW))
 	}
+	// get first 32 bytes of hashed pw
+	pwHash32 := []byte(hashedPW)[0:32]
 
-	sk := new(RSA.SecretKey)
-	sk.N = skN
-	sk.D = skD
+	// Get SK from file and unmarshal
+	jsonSK := AES.DecryptFromFile(filename, pwHash32)
+	var sk RSA.SecretKey
+	err := json.Unmarshal(jsonSK, &sk)
+	if err != nil {
+		panic("Wrong password, failed to unmarshal")
+	}
 
 	msgToBigInt := new(big.Int).SetBytes(msg)
 
-	signInt := RSA.Sign(*msgToBigInt, *sk)
+	signInt := RSA.Sign(*msgToBigInt, sk)
 
 	return signInt.String()
 }
